ea7781fde22af138b5906857955866897d966aac
jaz1492 <88153708+jaz1492@users.noreply.github.com>
2024-03-23 11:53:30 +0900

    Remove app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-const passwordChecker=function(){
-    const password = document.querySelector('#password').value;
-    const confirmPassword = document.querySelector('#confirm-password').value;
-    return console.log(password === confirmPassword? true : false);
-}
